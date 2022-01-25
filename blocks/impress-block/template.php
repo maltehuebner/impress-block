dbab5e9665ad1e1c3447b1ceddf7635742fb72f1
Malte Hübner <maltehuebner@gmx.org>
2022-01-25 22:48:29 +0100

    More WIP for template.

--- blocks/impress-block/template.php
+++ blocks/impress-block/template.php
@@ -1,15 +1,43 @@
-<?php declare( strict_types=1 ); ?>
+<?php declare( strict_types=1 );
+
+if (array_key_exists('remoteUrl', $attributes) && $attributes['remoteUrl']) {
+    $remoteUrl = $attributes['remoteUrl'];
+
+    $remoteJsonBody = wp_remote_get($remoteUrl)['body'];
+
+    $remoteProperties = json_decode($remoteJsonBody);
+
+    foreach ($remoteProperties as $key => $value) {
+        $ccKey = underscoreToCamelCase($key);
+
+        if (!array_key_exists($ccKey, $attributes) || !$attributes[$ccKey]) {
+            $attributes[$ccKey] = $remoteProperties->$key;
+        }
+    }
+}
+
+function underscoreToCamelCase($string, $capitalizeFirstCharacter = false): string
+{
+    $str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+
+    if (!$capitalizeFirstCharacter) {
+        $str[0] = strtolower($str[0]);
+    }
+
+    return $str;
+}
+?>
 
 <p>
-    Malte Hübner
+    <?php echo $attributes['firstName']; ?> <?php echo $attributes['lastName']; ?>
     <br />
-    Goethestraße 48
+    <?php echo $attributes['street']; ?> <?php echo $attributes['houseNumber']; ?>
     <br />
-    21335 Lüneburg
+    <?php echo $attributes['zipCode']; ?> <?php echo $attributes['city']; ?>
 </p>
 
 <p>
-    E-Mail: malte@caldera.cc
+    E-Mail: <a href="mailto:<?php echo $attributes['emailAddress']; ?>"><?php echo $attributes['emailAddress']; ?></a>
     <br />
-    Telefon: 041317073376
+    Telefon: <a href="tel:<?php echo $attributes['phoneNumber']; ?>"><?php echo $attributes['phoneNumber']; ?></a>
 </p>
